dcbbf8e73c22f9be4cf0622030c822da6bae376a
Anthony Kause <akause@barracudanetworks.com>
2013-06-08 23:54:08 -0400

    Removed ignored files

--- gen/com/akausejr/android/asyncintentservice/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.akausejr.android.asyncintentservice;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
